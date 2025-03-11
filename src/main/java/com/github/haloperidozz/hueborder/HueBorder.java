15a14150deb4959900e72441303831a54987d1f1
haloperidozz <196709604+haloperidozz@users.noreply.github.com>
2025-03-11 13:34:23 +0600

    style: fix indentation in HueBorder.java

--- src/main/java/com/github/haloperidozz/hueborder/HueBorder.java
+++ src/main/java/com/github/haloperidozz/hueborder/HueBorder.java
@@ -6,14 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class HueBorder implements ModInitializer {
-	public static final String MOD_ID = "hue-border";
+    public static final String MOD_ID = "hue-border";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		if (!WinVersion.isWindows11Build22000OrGreater()) {
-			LOGGER.error("unsupported system: Windows 11 (build >22000O) is required");
-		}
-	}
+    @Override
+    public void onInitialize() {
+        if (!WinVersion.isWindows11Build22000OrGreater()) {
+            LOGGER.error("unsupported system: Windows 11 (build >22000) is required");
+        }
+    }
 }
